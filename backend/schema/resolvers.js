081d9e06dda26fb0fd3e62f6153af87c3bea5544
Hasan_sh <hasansh343@gmail.com>
2018-11-01 08:33:50 +0100

    Convert to async await.

--- backend/schema/resolvers.js
+++ backend/schema/resolvers.js
@@ -20,33 +20,35 @@ const resolvers = {
     hi: () => 'Hi, there!'
   },
   Mutation: {
-    register: (parent, { username, company, country }, { models }) => {
-      return models.Register.findOne({ where: { username } })
-        .then(user => {
-          const validCountry = validateCountry(country)
-          if (user && !validCountry)
-            throw Error('User exists and not valid country.')
-          if (user) throw Error('User exists.')
-          if (!validCountry) throw Error('Country not valid')
+    register: async (parent, { username, company, country }, { models }) => {
+      try {
+        const validCountry = validateCountry(country)
+        if (!validCountry) throw Error('Country not valid')
 
-          return models.Register.create({ username, company, country }).then(
-            ({ dataValues }) => {
-              return {
-                error: null,
-                success: true,
-                time: dataValues.createdAt
-              }
-            }
-          )
+        const user = await models.Register.findOne({ where: { username } })
+        if (user && !validCountry)
+          throw Error('User exists and not valid country.')
+        if (user) throw Error('User exists.')
+
+        const { dataValues } = await models.Register.create({
+          username,
+          company,
+          country
         })
-        .catch(error => {
-          return {
-            error: error.message,
-            success: false,
-            time: Date.now()
-          }
-        })
+        return {
+          error: null,
+          success: true,
+          time: dataValues.createdAt
+        }
+      } catch (error) {
+        return {
+          error: error.message,
+          success: false,
+          time: Date.now()
+        }
+      }
     }
   }
 }
-module.exports = resolvers
+
+export default resolvers
