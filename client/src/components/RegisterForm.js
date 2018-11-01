537cac2b7401e3fdfb25b5f1c7f88e1f06757400
Hasan_sh <hasansh343@gmail.com>
2018-11-01 15:27:39 +0100

    Edit how to show the country field.

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import allowedEntries from '../allowedEntries'
 
 import './Register.css'
 
@@ -99,14 +100,14 @@ class RegisterForm extends Component {
               type="text"
               placeholder="Company"
             />
-            <input
-              value={country}
-              onChange={this.onChange}
-              required
-              name={'country'}
-              type="text"
-              placeholder="Country"
-            />
+            <select required name="country" onChange={this.onChange}>
+              <option value="">Select Country</option>
+              {allowedEntries.countries.map((country, i) => (
+                <option key={i} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
             <button className="registerBtn" type="submit">
               Register
             </button>
